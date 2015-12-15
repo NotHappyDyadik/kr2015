d0002f9a24c3eaba5ac0a09a2f0d001336cfd578
HappyDyadik <a.dyadik@yandex.ua>
2015-12-15 22:19:22 +0200

    Starting something

---
+++ Tanks/src/com/kyrsach/main/Main.java
@@ -0,0 +1,12 @@
+package com.kyrsach.main;
+
+import com.kyrsach.display.Display;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	Display.create(800, 600, "Tanks");	
+	}
+
+}
